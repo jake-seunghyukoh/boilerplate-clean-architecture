3ce37949a49c5ec0c7a4b8a85de08b6ba5be5719
Seunghyuk <seunghyukoh0@gmail.com>
2022-03-12 19:45:02 +0900

    Feat : App controller

--- src/InterfaceAdapters/controllers/controllers.module.ts
+++ src/InterfaceAdapters/controllers/controllers.module.ts
@@ -2,11 +2,12 @@ import { RepositoriesModule } from '@Adapters/repositories/repositories.module';
 import { UsersRepository } from '@Adapters/repositories/users.repository';
 import { Module } from '@nestjs/common';
 import { GetUsersInteractor } from '@UseCases/Interactors/users/getUsers.interactor';
+import { AppController } from './app.controller';
 import { UsersController } from './users.controller';
 
 @Module({
   imports: [RepositoriesModule],
-  controllers: [UsersController],
+  controllers: [AppController, UsersController],
   providers: [
     {
       provide: UsersController.GET_USERS_PORT,
